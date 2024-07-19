5fa4b363dfcb61f47f92446a4827c6c0aaab6aaf
zachcodesbots <zachhmltn@outlook.com>
2024-07-19 13:39:13 -0400

    Added functionality to quantity control reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload
+        const existingItem = state.items.find(item => item.name === name)
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({name, image, cost, quantity: 1})
+        }
     },
     removeItem: (state, action) => {
+        const existingItem = state.items.find(item => item.name === name)
+        if (existingItem) {
+            state.items = state.items.filter(item => item.name !== action.payload);
+        }
     },
     updateQuantity: (state, action) => {
-
-    
+        const {name, quantity} = action.payload;
+        const cartItem = state.items.find(item => item.name === name)
+        if (cartItem) {
+            cartItem.quantity = quantity;
+        }
     },
   },
 });
